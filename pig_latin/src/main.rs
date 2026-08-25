6fa446ac36fb8797c4822bede0280301e96af579
aswitocom <aswitocom@gmail.com>
2026-08-25 07:48:43 +0300

    take word, vowel or consonant from std input print pig latin version

--- pig_latin/src/main.rs
+++ pig_latin/src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Convert Words to Pig Latin ....\n");
+
+    let mut word = String::new();
+
+    io::stdin()
+        .read_line(&mut word)
+        .expect("Failed to get word");
+
+    let word = word.trim().to_string();
+
+    let new_word = word_to_pig_latin(&word);
+
+    println!("{}", &new_word);
+}
+
+fn word_to_pig_latin(word: &str) -> String {
+    let vowels = vec!['a', 'e', 'i', 'o', 'u'];
+
+    if word.starts_with(|v: char| vowels.contains(&v)) {
+        let word = format!("{word}hay");
+        word
+    } else {
+        let mut chars = word.chars();
+
+        let new_word = match chars.next() {
+            None => String::new(),
+            Some(first) => {
+                let mut rest_of_word: String = chars.collect();
+                rest_of_word.push(first);
+                rest_of_word
+            }
+        };
+
+        let word = format!("{new_word}ay");
+        word
+    }
 }
